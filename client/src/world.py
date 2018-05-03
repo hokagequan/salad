fb41d0ef84753178db2c9818ef5cc566510f657d
Quan <hokagequan@me.com>
2018-05-03 23:22:10 +0800

    send card

--- client/src/world.py
+++ client/src/world.py
@@ -10,17 +10,29 @@ class World(object):
 	rules = Rule()
 	all_cards = rules_contents.all_cards
 	persons = []
+	# Current person index
+	cur_p_index = -1
 
 	def __init__(self):
 		super(World, self).__init__()
 		self.prepare()
+		self.start()
 
 	def prepare(self):
 		# Shuffle
 		self.shuffle_cards()
 		# test
 		persons = [models.Person] * 3
+		cur_p_index = random.randint(0, len(persons))
 
+	def get_next_person(self):
+		self.cur_p_index += 1
+		self.cur_p_index = cur_p_index if cur_p_index < len(persons) else 0
+		return persons[self.cur_p_index]
+		 
+	# 发牌
+	def send_cards(self):
+		
 
 	'''[summary]
 	Shuffle
@@ -36,8 +48,9 @@ class World(object):
 	Start
 	[description]
 	'''
-	def start():
+	def start(self):
 		# todo
+		self.send_cards()
 		pass
 
 
