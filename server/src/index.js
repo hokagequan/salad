d02610451b8b6fc34d2791cf973dfa6ee11ea876
QiM <hokagequan@me.com>
2018-05-17 14:19:42 +0800

    基本图片

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,7 @@ import controller from './controllers'
 let app = express();
 
 app.use(express.static(__dirname + "/views"));
+app.use(express.static(__dirname + "/public"));
 app.use('/', controller);
 
 app.listen('3000', function(){
