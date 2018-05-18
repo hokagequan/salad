99961f10e1fb8251be389cb8a5420617fca12fbb
QiM <hokagequan@me.com>
2018-05-18 21:26:14 +0800

    倒计时

--- server/src/views/game/src/main.js
+++ server/src/views/game/src/main.js
@@ -1,12 +1,19 @@
 
 let resources = ["game/assets/images/treasureHunter.json"];
 let g = hexi(512, 512, setup, resources, load);
-g.backgroundColor = 0x000000;
+g.backgroundColor = 0x5a96c6;
 g.scaleToWindow();
 g.start();
 
+// 出牌倒计时label
+let timer = g.text("60", "36px puzzler", "white");
+timer.visible = true;
+
 function setup() {
-	
+	timer.x = g.stage.width / 2;
+	timer.y = 30
+	// timer.poistion.set(g.stage.width / 2, 30);
+
 	g.state = play;
 }
 
